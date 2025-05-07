5d42ca6791a5e444f9f7e263b00ab5a5bbfd7cff
Akshat Pratyush <akshatp0505@gmail.com>
2025-05-08 01:29:38 +0530

    Created My Product card and basic grid

--- client/src/components/ProductCards.tsx
+++ client/src/components/ProductCards.tsx
@@ -1,5 +1,31 @@
-const ProductCards = () => {
-  return <div></div>;
+interface Product {
+  name: string;
+  desc: string;
+  price: string;
+  image?: string;
+}
+
+interface ProductCardsProps {
+  products: Product;
+}
+
+const ProductCards = ({ products }: ProductCardsProps) => {
+  return (
+    <div className="max-w-md mx-auto bg-white rounded-lg shadow-md p-4">
+      <img
+        src={
+          products.image
+            ? products.image
+            : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNNLEL-qmmLeFR1nxJuepFOgPYfnwHR56vcw&s"
+        }
+        alt={products.name}
+        className="w-full h-48 object-cover rounded-md mb-4"
+      />
+      <h2 className="text-xl font-semibold mb-2">{products.name}</h2>
+      <p className="text-gray-600 mb-2">{products.desc}</p>
+      <p className="text-blue-600 font-bold">₹{products.price}</p>
+    </div>
+  );
 };
 
 export default ProductCards;
--- client/src/pages/MyProducts.tsx
+++ client/src/pages/MyProducts.tsx
@@ -1,9 +1,44 @@
-const MyProducts = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import { useEffect } from "react";
+import ProductCards from "../components/ProductCards";
 
-export default MyProducts
+const MyProducts = () => {
+  useEffect(() => {
+    console.log("====================================");
+    console.log("MyProducts page loaded");
+    console.log("====================================");
+  }, []);
+  return (
+    <div className="p-5 w-[80%] mx-auto">
+      <div className="">Search bar to be made here</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <ProductCards
+          products={{
+            name: "a",
+            desc: "aa",
+            price: "452",
+            image:
+              "https://www.godrejinterio.com/imagestore/B2C/56101522SD04105/56101522SD04105_01_1500x1500.jpg",
+          }}
+        />
+        <ProductCards
+          products={{
+            name: "a",
+            desc: "aa",
+            price: "452",
+          }}
+        />
+        <ProductCards
+          products={{
+            name: "a",
+            desc: "aa",
+            price: "452",
+            image:
+              "https://www.godrejinterio.com/imagestore/B2C/56101522SD04105/56101522SD04105_01_1500x1500.jpg",
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MyProducts;
