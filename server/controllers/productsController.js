aebb19341277b57f502929cd59ddb4cae4f21865
Akshat Pratyush <akshatp0505@gmail.com>
2025-05-08 15:54:21 +0530

    Added success in response

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -12,22 +12,23 @@ const postProduct = async (req, res) => {
     );
 
     res.status(201).json({
+      success: true,
       message: "Product added successfully",
       product: result.rows[0],
     });
   } catch (error) {
     console.error("Error inserting product:", error);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ success: false, error: "Internal server error" });
   }
 };
 
 const getAllProducts = async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM products;");
-    res.status(200).json({ products: result.rows });
+    res.status(200).json({ success: true, products: result.rows });
   } catch (error) {
     console.error("Error fetching products:", error);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ success: false, error: "Internal server error" });
   }
 };
 
@@ -39,10 +40,10 @@ const getProducts = async (req, res) => {
       "SELECT * FROM products WHERE title ILIKE $1;",
       [`%${title}%`]
     );
-    res.status(200).json({ products: result.rows });
+    res.status(200).json({ success: true, products: result.rows });
   } catch (error) {
     console.error("Error fetching products:", error);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ success: false, error: "Internal server error" });
   }
 };
 
